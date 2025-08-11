baa4d905940e9541225aa42fa03edd933c75cf96
Tejas Rajput <imtejas360@gmail.com>
2025-08-11 11:04:33 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -5,6 +5,6 @@ import pandas as pd
 
 def clean_data(file):
     data=pd.read_excel(file)
-    return data.assign(FirstName=lambda df:df.Name.str.strip().str.split(' ',),Email=lambda df:df.Email.str.strip())
+    return data.assign(FirstName=lambda df:df.Name.str.strip().str.split(' '),Email=lambda df:df.Email.str.strip())
 
 
