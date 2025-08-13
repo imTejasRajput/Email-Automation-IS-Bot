f7895c4fa19f1c63acbd692c9f3449a2b95cc86a
Tejas Rajput <imtejas360@gmail.com>
2025-08-13 11:07:35 +0530

    Update emailcreator.py

--- emailcreator.py
+++ emailcreator.py
@@ -7,5 +7,5 @@ def ai_email_creator(name,magazine_title,edition_title,price,about,experience=No
     personlised_response=create_response(about,experience)
     return AI_TEMPLATE.format(name=name,magazine_title=magazine_title,edition_title=edition_title,price=price,personlised_response=personlised_response)
 
-def test_template_creator():
+def test_email_creator():
     return TEST_TEMPLATE
